afb1d7a5f94eda779bd7a30b99f033fc554e5784
Jacob Prebys <jacobprebys@gmail.com>
2023-01-09 19:53:52 -0600

    Update .gitignore; add socket stuff

--- src/server.c
+++ src/server.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	int socket_desc;
+
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (socket_desc == -1)
+	{
+		perror("initializing socket");
+		exit(EXIT_FAILURE);
+	}
+
 	printf("Hello, World!\n");
 	return 0;
 }
